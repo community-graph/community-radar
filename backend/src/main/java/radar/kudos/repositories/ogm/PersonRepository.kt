02e2b5661bb538720225cc26d06e5fdf82279345
Gerrit Meier <gerrit.meier@neo4j.com>
2017-10-30 16:49:13 +0100

    Remove old classes.

--- backend/src/main/java/radar/kudos/repositories/ogm/PersonRepository.kt
+++
@@ -1,9 +0,0 @@
-package radar.kudos.repositories.ogm
-
-import radar.kudos.domain.model.persistent.entities.ogm.Person
-import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.stereotype.Repository
-
-
-@Repository
-interface PersonRepository : PagingAndSortingRepository<Person, String>
