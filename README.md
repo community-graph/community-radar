ccecd9e98d0f0f42ca7f45e1118305af92e8517a
Jasper Blues <jasper@appsquick.ly>
2017-10-30 10:39:00 +0800

    Remove API key from integration test - not needed. Readme notes. Layout in kudos UI.

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # community-radar
 A simple display of community activity from the community graph implemented in Kotlin
 
+## Getting set up
+
+Data from Neo4j is enriched in real-time using Twitter APIs, which require access credentials. 
+
+* Obtain Twitter API creds from <a href="https://apps.twitter.com/">apps.twitter.com</a>
+* Register a new application, once done, edit `backend/src/main/resources/application.properties`
+
+```properties
+twitter.consumerKey=$$YOUR_CONSUMER_KEY_HERE$$
+twitter.consumerSecret=$$YOUR_CONSUMER_SECRET_HERE$$
+twitter.accessKey=$$YOUR_OAUTH_KEY_HERE$$
+twitter.accessSecret=$$YOUR_OAUTH_SECRET_HERE$$
+```
 
 ## Running the backend
 
--- backend/src/test/java/kudos/api/endpoints/pub/KudosControllerIntegrationTests.kt
+++ backend/src/test/java/kudos/api/endpoints/pub/KudosControllerIntegrationTests.kt
@@ -3,19 +3,13 @@ package kudos.api.endpoints.pub
 import com.jayway.restassured.RestAssured
 import kudos.api.endpoints.ControllerTest
 import org.junit.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 
 class KudosControllerIntegrationTests : ControllerTest() {
-
-    @Autowired
-    @Value("@{api.key}") lateinit var apiKey: String
 
     @Test
     fun shouldReturnForSpecifiedId() {
 
         RestAssured.given()
-                .header("API-Key", apiKey)
                 .header("content-type", "application/json")
                 .get("/kudos/for/abreslav").peek().then()
                 .assertThat().statusCode(200)
@@ -25,7 +19,6 @@ class KudosControllerIntegrationTests : ControllerTest() {
     fun shouldReturnRandom() {
 
         RestAssured.given()
-                .header("API-Key", apiKey)
                 .header("content-type", "application/json")
                 .get("/kudos/random").peek().then()
                 .assertThat().statusCode(200)
