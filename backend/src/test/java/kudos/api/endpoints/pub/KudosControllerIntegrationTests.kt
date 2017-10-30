7396a88352f78a4961a2582df2ca3d8e324e9932
Jasper Blues <jasper@appsquick.ly>
2017-10-30 10:54:32 +0800

    Remove API key from integration test - not needed. Readme notes. Layout in kudos UI. (#4)

--- backend/src/test/java/kudos/api/endpoints/pub/KudosControllerIntegrationTests.kt
+++ backend/src/test/java/kudos/api/endpoints/pub/KudosControllerIntegrationTests.kt
@@ -3,8 +3,6 @@ package kudos.api.endpoints.pub
 import com.jayway.restassured.RestAssured
 import kudos.api.endpoints.ControllerTest
 import org.junit.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 
 class KudosControllerIntegrationTests : ControllerTest() {
 
