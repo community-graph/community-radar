94d5b12450fe66a40ed2bdf9bfb6b5b514a60fcc
Jasper Blues <jasper@appsquick.ly>
2017-11-01 06:37:27 +0800

    Drop back to stable spring versions.

--- backend/src/test/java/radar/ControllerTest.kt
+++ backend/src/test/java/radar/ControllerTest.kt
@@ -2,12 +2,11 @@ package radar
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.jayway.restassured.RestAssured
-import radar.ApplicationConfig
 import org.junit.Before
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
